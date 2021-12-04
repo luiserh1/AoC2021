5eeb146ec94584af87a96239f6ea85f1bb7a92ac
Luis Serrano Hernández <luiserh1@inf.upv.es>
2021-12-04 21:13:57 +0100

    Part B Updated the code for reuse Part B is generalized, so it can also solve Part A

--- Day_1/main.py
+++ Day_1/main.py
@@ -1,23 +1,53 @@
-def main():
-	# Read and process the input
-	data = []
-	with open("input.txt", "rt") as f:
-		lines = f.readlines()
-		data = [int(depth_m) for depth_m in lines]
+def print_data(data, num):
 	num_measurements = len(data)
-
-	# Print some data
-	print("First 10 (from a total of {}) measurements:".format(num_measurements))
-	for depth_m in data[:10]:
+	num = min(num, num_measurements)
+	print("First {} (from a total of {}) measurements:".format(num, num_measurements))
+	for depth_m in data[:num]:
 		print(depth_m)
 
-	# Do the calculations
-	increases = 0
-	for i in range(1, num_measurements):  # REMEMBER: The second parameter is not included!
-		increases += (1 if data[i] > data[i-1] else 0)
 
-	# Print  the solution
-	print("======================\nSOL: [{}] increases out of {} steps".format(increases, num_measurements - 1))
+def get_processed_input(input_path, data):
+	with open(input_path, "rt") as f:
+		lines = f.readlines()
+		data += [int(depth_m) for depth_m in lines]
+		num_measurements = len(data)
+		print_data(data, 10)
+
+
+def part_a(data):
+	increases = 0
+	for i in range(1, len(data)):
+		increases += (1 if data[i] > data[i-1] else 0)
+	return increases
+
+
+def part_b(data, window_size):
+	increases = 0
+	if window_size > len(data):
+		return increases
+	window_sum = sum(data[:window_size])
+	for i in range(window_size, len(data)):
+		last_window_sum = window_sum
+		window_sum += data[i] - data[i - window_size]
+		increases += (1 if window_sum > last_window_sum else 0)
+	return increases
+
+
+def main():
+	data = []
+	input_path = "input.txt"
+	get_processed_input(input_path, data)
+	num_measurements = len(data)
+
+	sol_a = part_a(data)
+	print("======================\nSOL A: [{}] increases out of {} steps".format(sol_a, num_measurements - 1))
+
+	window_size = 3
+	sol_b = part_b(data, window_size)
+	print("======================\nSOL B: [{}] increases out of {} steps with a {}-sized window".format(
+		sol_b, num_measurements -window_size , window_size))
+
+
 
 
 if __name__=="__main__":
